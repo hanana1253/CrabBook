a952e9065349011210ca9d9b858c9ea32e856512
sosoYim <sohyung.yim@gmail.com>
2021-11-01 19:48:32 +0900

    feat: Put sample chart using store obj

--- src/js/components/statistics.js
+++ src/js/components/statistics.js
@@ -1,13 +1,12 @@
 // import store from '../store/store-test.js';
 // DOM Nodes
+const profilChart = document.querySelector('.profil__chart').getContext('2d');
 
 // state, 링크 데이터(임시)
 let state = [];
 
 // Functions
-const countScrapsByCategory = () => {
-  [...state].map(({ items }) => items.length);
-};
+const countScrapsByCategory = () => state.map(({ items }) => items.length);
 
 // const getCategoriesTitle = () => {
 //   state.map(({ title }) => title);
@@ -29,33 +28,34 @@ const countScrapsByCategory = () => {
 
 // TODO: generateColors
 // 랜덤컬러? 사용자 지정?
-
-const pieChart = document.querySelector('.profil__chart').getContext('2d');
-const pieScrapsByCategory = new Chart(pieChart, {
-  type: 'pie',
-  data: {
-    labels: state.map(({ title }) => title),
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: countScrapsByCategory(),
-        backgroundColor: [
-          // TODO: generateColors
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }
-    ]
-  }
-});
+const renderChart = (type, data) => new Chart(type, data);
 
 // Event
 window.addEventListener('DOMContentLoaded', async () => {
   try {
     const { data: newState } = await axios.get('/state');
     state = newState;
+
+    // TODO: 함수 빼기
+    renderChart(profilChart, {
+      type: 'doughnut',
+      data: {
+        labels: state.map(({ title }) => title),
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: countScrapsByCategory(),
+            backgroundColor: [
+              // TODO: generateColors
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+          }
+        ]
+      }
+    });
   } catch (e) {
     console.error(e);
   }
