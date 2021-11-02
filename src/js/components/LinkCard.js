7bdd2327dc587523c942f23225d3c3de3ce7b7d0
Hoya-kim <1203play@gmail.com>
2021-11-02 16:38:47 +0900

    feat: Implement LinkCard component except tag's feature

--- src/js/components/LinkCard.js
+++ src/js/components/LinkCard.js
@@ -1,15 +1,3 @@
-const bindEvent = $target => {
-  $target.onclick = e => {
-    if (
-      e.target.matches('.linkcard__thumbnail') ||
-      e.target.matches('.linkcard__content__title')
-    )
-      return;
-    $target.classList.toggle('linkcard--active');
-  };
-  return $target;
-};
-
 export default class LinkCard {
   constructor(linkData) {
     this._id = linkData.id;
@@ -30,27 +18,50 @@ export default class LinkCard {
 
   set memo(newMemo) {
     this._memo = newMemo;
+    // @todo: request post to server
+    // this.updateMemo();
   }
 
+  async updateMemo() {
+    const response = await axios.post('/url', {
+      id: this._id,
+      memo: this._memo
+    });
+    if (response.status === 200) return;
+    // @todo: error handling
+  }
+
+  /**
+   * Get card DOM Element
+   * @returns {DOMElement}
+   */
   get cardElement() {
-    const $linkCard = this.createLinkCard();
-    return bindEvent($linkCard);
+    return this.createLinkCard();
   }
 
-  createLinkCard() {
-    const $linkCard = document.createElement('article');
-    $linkCard.classList.add('linkcard');
-
+  /**
+   * Create link card's Thumbnail
+   * @returns {DOMElement}
+   */
+  createLinkThumbnail() {
     const $thumbnail = document.createElement('figure');
     $thumbnail.classList.add('linkcard__thumbnail');
-    $thumbnail.onclick = () => window.open(this._url, '_blank');
     $thumbnail.innerHTML = `
-      <img class="linkcard__thumbnail--img"
+      <img class="linkcard__thumbnail__img"
           src="${this._img.url}"
           alt="${this._title}"
       />`;
-    $linkCard.appendChild($thumbnail);
+    // Add Event linstener
+    $thumbnail.onclick = () => window.open(this._url, '_blank');
+    return $thumbnail;
+  }
 
+  /**
+   * Create link card's Details
+   * @param {DOMElement} $targetCard
+   * @returns {DOMElement}
+   */
+  createLinkDetail($targetCard) {
     const $linkInfo = document.createElement('div');
     $linkInfo.classList.add('linkcard__content');
     $linkInfo.innerHTML = `
@@ -59,28 +70,107 @@ export default class LinkCard {
       </a>
       <div class="linkcard__content__tag__container">
         ${this._tags.map(tag => `<button class="badge">${tag}</button>`)}
+        <button class="badge add-badge">+</button>
       </div>
       <div class="linkcard__content__description">
         ${this._description}
-      </div>
-      <div class="linkcard__content__memo">
-        <div><i>memo</i></div>
-        <textarea class="linkcard__content__memo__textarea">${
-          this._memo
-        }</textarea>
       </div>
       <button class="toggle-card">
         <i class="bx bx-dots-vertical-rounded"></i>
         <div class="tooltipMenu">
           <ul>
-            <li>Delete</li>
-            <li>Favarite</li>
+            <li class="linkcard__show-all">Show All</li>
+            <li class="linkcard__delete">Delete</li>
           </ul>
         </div>
       </button>
-      
     `;
-    $linkCard.appendChild($linkInfo);
+
+    $linkInfo
+      .querySelector('.linkcard__show-all')
+      .addEventListener('click', e => {
+        const isActive = $targetCard.classList.toggle('linkcard--active');
+        isActive
+          ? (e.target.textContent = 'Hide')
+          : (e.target.textContent = 'Show All');
+      });
+    $linkInfo
+      .querySelector('.linkcard__delete')
+      .addEventListener('click', () => {
+        $targetCard.remove();
+      });
+    return $linkInfo;
+  }
+
+  /**
+   * Create link card's memo
+   * @param {DOMElement} $targetCard
+   * @returns {DOMElement}
+   */
+  createLinkMemo($targetCard) {
+    const $linkMemo = document.createElement('div');
+    $linkMemo.classList.add('linkcard__content__memo');
+    $linkMemo.innerHTML = `<div class="linkcard__content__memo__title"><i>memo</i></div>`;
+
+    const $textarea = document.createElement('textarea');
+    $textarea.setAttribute('readonly', true);
+    $textarea.classList.add('linkcard__content__memo__textarea');
+    $textarea.value = this.memo;
+    $linkMemo.appendChild($textarea);
+
+    const $editButton = document.createElement('button');
+    $editButton.classList.add('edit__button');
+    $editButton.innerHTML = `<i class="bx bx-edit"></i>edit`;
+    $linkMemo.appendChild($editButton);
+
+    // Add Event linstener
+    let isShift = false;
+    $textarea.onkeyup = e => {
+      if (e.key === 16) isShift = false;
+    };
+
+    $textarea.onkeydown = e => {
+      if (e.key === 'Shift') isShift = true;
+      if (e.key === 'Enter' && isShift) {
+        this.memo = $textarea.value;
+        $targetCard
+          .querySelector('.linkcard__show-all')
+          .dispatchEvent(new Event('click'));
+        $editButton.innerHTML = '<i class="bx bx-edit"></i>edit';
+      }
+    };
+
+    $editButton.onclick = () => {
+      $textarea.toggleAttribute('readonly');
+      if ($textarea.getAttribute('readonly') === null) {
+        $editButton.innerHTML = '<i class="bx bx-edit"></i>done';
+        $textarea.focus();
+      } else {
+        this.memo = $textarea.value;
+        $targetCard
+          .querySelector('.linkcard__show-all')
+          .dispatchEvent(new Event('click'));
+        $editButton.innerHTML = '<i class="bx bx-edit"></i>edit';
+      }
+    };
+
+    return $linkMemo;
+  }
+
+  /**
+   * Create Link Card Element
+   * @returns {DOMElement}
+   */
+  createLinkCard() {
+    const $linkCard = document.createElement('article');
+    $linkCard.classList.add('linkcard');
+
+    // Add Thumbnail
+    $linkCard.appendChild(this.createLinkThumbnail());
+    // Add Details
+    $linkCard.appendChild(this.createLinkDetail($linkCard));
+    // Add Memo
+    $linkCard.appendChild(this.createLinkMemo($linkCard));
 
     return $linkCard;
   }
