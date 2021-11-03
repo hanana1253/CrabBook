37095c19108acce358052b363b693b1f80d6d802
Hoya-kim <1203play@gmail.com>
2021-11-03 10:56:49 +0900

    feat: Add paste event template for binding later

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -155,3 +155,22 @@ $form.onsubmit = e => {
   addLink($input.value);
   $input.value = '';
 };
+
+// window.addEventListener('paste', e => {
+//   // Stop data actually being pasted into div
+//   e.stopPropagation();
+//   e.preventDefault();
+
+//   // Get pasted data via clipboard API
+//   const clipboardData = e.clipboardData || window.clipboardData;
+//   const pastedData = clipboardData.getData('Text');
+
+//   if (!validUrlRegExp.test(pastedData)) {
+//     alert('유효하지 않은 URL입니다.');
+//     return;
+//   }
+
+//   // @todo: Do something
+//   alert(`붙여 넣은 데이터: ${pastedData}`);
+//   // addLink(pastedData);
+// });
